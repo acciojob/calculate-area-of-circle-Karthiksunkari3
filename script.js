0467c3f116b2ca15c22500337daf4eb5e4145c0f
Karthik Sunkari <84893245+Karthiksunkari3@users.noreply.github.com>
2023-06-30 00:07:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+function calculateCircleArea() {
+  const radius = parseFloat(prompt("Enter the radius of the circle:")); // Take input from the user
+
+  if (isNaN(radius)) {
+    console.log("Invalid input. Please enter a valid number."); // Handle invalid input
+    return;
+  }
+
+  const area = Math.PI * radius * radius; // Calculate the area of the circle
+  const roundedArea = area.toFixed(2); // Round off the area to two decimal places
+
+  console.log("The area of the circle with radius", radius, "is", roundedArea);
+}
+
+calculateCircleArea();
